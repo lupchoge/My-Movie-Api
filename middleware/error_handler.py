1c98463a810425325c861edda156bfec8eea51d8
lupchoge <flippynt25@gmail.com>
2023-05-28 20:34:06 -0500

    Update error_handler.py

--- middleware/error_handler.py
+++ middleware/error_handler.py
@@ -1,14 +1,13 @@
 from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import JSONResponse
-from typing import Union
 
 class ErrorHandler(BaseHTTPMiddleware):
     def __init__(self, app: FastAPI) -> None: # Crear metodo constructor que recibe la app q es de tipo FastAPI
         super().__init__(app)
 
     # Metodo para detectar errores en la app
-    async def dispatch(self, request: Request, call_next) -> Union[Response, JSONResponse]:
+    async def dispatch(self, request: Request, call_next) -> Response | JSONResponse:
         try:
             #si no ocurre ningun error pasa a la siguiente llamada (call_next)
             return await call_next(request)
